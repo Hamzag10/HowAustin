19085774d71e4c1c8ddf53b35c38771235cbd1fa
renuf <renuf@users.noreply.github.com>
2015-09-26 23:21:20 -0500

    Pushing blank template

---
+++ src/app.js
@@ -0,0 +1,17 @@
+/**
+ * Welcome to Pebble.js!
+ *
+ * This is where you write your app.
+ */
+
+var UI = require('ui');
+//var Vector2 = require('vector2');
+
+var main = new UI.Card({
+  title: 'Pebble.js',
+  icon: 'images/menu_icon.png',
+  subtitle: 'Hello World!',
+  body: 'Press any button.'
+});
+
+main.show();
