d1fa19dee4c2294818bc4b8df150d653b759f80e
Hamzag10 <hamzag10@gmail.com>
2015-09-26 23:41:25 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -8,10 +8,46 @@ var UI = require('ui');
 //var Vector2 = require('vector2');
 
 var main = new UI.Card({
-  title: 'Pebble.js',
+  title: 'A hip tour guide to AUstin',
   icon: 'images/menu_icon.png',
-  subtitle: 'Hello World!',
-  body: 'Press any button.'
+  subtitle: 'Welcome to ATX!',
+  body: 'Please press selecct for more!.'
 });
 
 main.show();
+
+var wind = new UI.Window({ fullscreen: true });
+var image = new UI.Image({
+  position: new Vector2(0, 0),
+  size: new Vector2(144, 96),
+  image: 'IMAGE HERE DAWG'
+});
+
+wind.add(image);
+
+wind.show();
+wind.on('click', 'Select', wind.hide);
+
+main.on('click', 'up', function(e) {
+  var menu = new UI.Menu({
+    sections: [{
+      items: [{
+        title: 'UT Tower',
+        subtitle: ''
+      }, 
+      {
+        title: 'Capital',
+        subtitle: ''
+      },
+      {
+        title:'Barton Springs',
+      }
+      ]
+    }]
+  });
+  menu.on('select', function(e) {
+    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
+    console.log('The item is titled "' + e.item.title + '"');
+  });
+  menu.show();
+});
